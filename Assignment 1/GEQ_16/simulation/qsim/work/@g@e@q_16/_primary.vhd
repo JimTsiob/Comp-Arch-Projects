113828c8c02f99021c2b90c7e4a6ef235337ccdc
Dimitris Tsiompikas <53694372+JimTsiob@users.noreply.github.com>
2022-04-04 21:41:28 +0300

    Delete Assignment 1/GEQ_16 directory

--- Assignment 1/GEQ_16/simulation/qsim/work/@g@e@q_16/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity GEQ_16 is
-    port(
-        in1             : in     vl_logic_vector(15 downto 0);
-        output          : out    vl_logic_vector(15 downto 0)
-    );
-end GEQ_16;
