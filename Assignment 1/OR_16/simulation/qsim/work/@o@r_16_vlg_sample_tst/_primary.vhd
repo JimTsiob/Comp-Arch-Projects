9ad2c7b021497f8f70040eb1a35795683f7323b1
Dimitris Tsiompikas <53694372+JimTsiob@users.noreply.github.com>
2022-04-04 21:41:47 +0300

    Delete Assignment 1/OR_16 directory

--- Assignment 1/OR_16/simulation/qsim/work/@o@r_16_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity OR_16_vlg_sample_tst is
-    port(
-        in1             : in     vl_logic_vector(15 downto 0);
-        in2             : in     vl_logic_vector(15 downto 0);
-        sampler_tx      : out    vl_logic
-    );
-end OR_16_vlg_sample_tst;
