1c2e084af812e1389ea0337094e046e5f8540539
Tsiobieman <dtsiomp@hotmail.com>
2022-05-24 16:46:07 +0300

    Added all needed 3rd assignment files

--- Assignment 3/VHDL Files/CONTROL.vhd
+++ Assignment 3/VHDL Files/CONTROL.vhd
@@ -18,39 +18,54 @@ END CONTROL;
 
 ARCHITECTURE behavioral of CONTROL is
 BEGIN
+
+
 	get_result : process(flush,func,opcode) begin
-		if flush = '1' then
+	
+		--DEFAULT VALUES WHEN CONTROL STARTS
+		--AND WHEN FLUSH IS ENABLED
+		
 			isR <= '0';
 			isMPFC <= '0';
 			isLW <= '0';
 			isSW <= '0';
 			isBranch <= '0';
 			isReadDigit <= '0';
+			isPrintDigit <= '0';
 			isJumpD <= '0';
 			isJR <= '0';
-		end if;
 		
 		if flush = '0' then
+		
 			case opcode is
 				when "0000" =>
 					isR <= '1';
 					if func = "111" then
 						isMPFC <= '1';
 					end if;
+					
+					
 				when "0001" =>
 					isLW <= '1';
-				When "0010" =>
+					
+				when "0010" =>
 					isSW <= '1';
+					
 				when "0100" =>
 					isBranch <= '1';
+					
 				when "0110" =>
 					isReadDigit <= '1';
+					
 				when "0111" =>
 					isPrintDigit <= '1';
+					
 				when "1111" =>
 					isJumpD <= '1';
+					
 				when "1101" =>
 					isJR <= '1';
+					
 				when others =>
 					isR <= '0';
 			end case;
